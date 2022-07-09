1b249c2c55b765df37a5ed5f4ce646f2a5f8586f
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-07-09 15:30:12 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -223,7 +223,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "自定义jar加载成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "&( ^___^ )&，加载成功！", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
@@ -240,7 +240,7 @@ public class HomeActivity extends BaseActivity {
                         mHandler.post(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(HomeActivity.this, "jar加载失败", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(HomeActivity.this, "&（┬＿┬ )&，加载失败，请重试。", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         });
