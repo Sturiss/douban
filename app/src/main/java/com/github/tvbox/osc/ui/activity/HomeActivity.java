2d481ba4c2f771d7b51d659e6a795027c0ccc6c5
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-07-11 10:49:49 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -223,7 +223,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "配置加载成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "自定义配置加载成功", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
